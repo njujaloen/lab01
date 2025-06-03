e4fe4265e56cd047a2a49a1ca4347b0bc6033526
njujaloen <njujaloen@augusta.edu>
2025-06-02 23:35:00 -0400

    Add files via upload

---
+++ CSLab1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Worksheet1
+{
+    public static void Main(string[] args)
+    {
+    
+        Console.WriteLine("Coding is fun!");
+
+   
+        string message = "Coding is fun!";
+        Console.WriteLine(message);
+
+   
+        int studentId = 12345;
+        Console.WriteLine(studentId);
+
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        Console.WriteLine("The sum is " + sum);
+    }
+}
